597078993b46126985547dd8d24867b0e6788802
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2026-03-10 19:00:57 +0900

    login update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Join from "./pages/Join";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
+import Join from "./pages/Join";
 import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main/>}></Route>
-        <Route path="/" element={<Join/>}></Route>
-        <Route path="/" element={<Login/>}></Route>
-      </Routes>  
+        <Route path="/" element={<Main />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+    //  페이지 이동 함수 
+    const navigate = useNavigate();
   const login = async () => {
     try {
       const response = await fetch("http://localhost:8080/api/member/login", {
@@ -25,6 +27,8 @@ function Login() {
       const data = await response.json();
       console.log(data);
       alert("로그인 성공");
+      // 메인 페이지로 이동 
+      navigate("/");
     } catch (error) {
       console.error(error);
       alert("서버 연결 실패");
@@ -35,22 +39,9 @@ function Login() {
     <div>
       <h2>로그인</h2>
 
-      <input
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="이메일"
-      />
-      <br />
-      <br />
+      <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="이메일"/><br /><br />
 
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="비밀번호"
-      />
-      <br />
-      <br />
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="비밀번호"/><br /><br />
 
       <button onClick={login}>로그인</button>
     </div>

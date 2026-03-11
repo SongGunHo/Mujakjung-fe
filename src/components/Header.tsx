b528ca0eef6debb6779fabab551ad36324cc84c9
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2026-03-11 22:42:39 +0900

    logo save

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,56 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-function Header() {
-    // 페이지 이동 함수 
-    const navigte = useNavigate();
-    // 검색어 상태 
-    const [keyword, setKeyword] = useState("");
-    // 여행 타입 상태 (국내 / 해오ㅣ)
-    const [type , setType] = useState("domestic");
+import { useNavigate, Link } from "react-router-dom";
+import logo from "../assets/Mujakjung.jpg";
 
-    // 검색 버튼 클릭 
-    const search = () =>{
-    // 검색 페이지 이동
-        navigte(`/search?keyword=${keyword}&type=${type}`)
-    }
+function Header() {
+
+  // 페이지 이동 함수
+  const navigate = useNavigate();
+
+  // 검색어 상태
+  const [keyword, setKeyword] = useState("");
+
+  // 여행 타입 상태
+  const [type, setType] = useState("domestic");
+
+  // 검색 버튼 클릭
+  const search = () => {
+    navigate(`/search?keyword=${keyword}&type=${type}`);
+  };
+
   return (
     <header>
-    {/** 사이트 제목 */}
+
+      {/* 로고 클릭하면 메인 이동 */}
+      <Link to="/">
+        <img src={logo} alt="Mujakjung 로고" width="80" />
+      </Link>
+
+      {/* 사이트 제목 */}
       <h1>AI 여행 사이트</h1>
-    {/** 검색어 입력 */}
-    <input type="text" value={keyword} onChange={(e)=> setKeyword(e.target.value)} placeholder="검색어를 입력을 하새요" />
-    {/** 검색 버튼  */}
-    <select value={type} onChange={(e)=> setType(e.target.value)}>
-        <option value="domestic">국내 여행 </option>
+
+      {/* 검색어 입력 */}
+      <input
+        type="text"
+        value={keyword}
+        onChange={(e)=> setKeyword(e.target.value)}
+        placeholder="검색어를 입력하세요"
+      />
+
+      {/* 여행 타입 */}
+      <select value={type} onChange={(e)=> setType(e.target.value)}>
+        <option value="domestic">국내 여행</option>
         <option value="overseas">해외 여행</option>
-    </select>
-    {/** 검색 버튼  */}
-    <button onClick={search}>검색</button>
-    {/** 회원 가입 페이지 이동  */}
-    <button onClick={()=> navigte("/join")}>회원 가입</button>
-    {/** 로그인 페이지 이동 */}
-    <button onClick={()=> navigte("login")}>로그인</button>
+      </select>
 
+      {/* 검색 버튼 */}
+      <button onClick={search}>검색</button>
 
+      {/* 회원가입 */}
+      <button onClick={()=> navigate("/join")}>회원 가입</button>
 
+      {/* 로그인 */}
+      <button onClick={()=> navigate("/login")}>로그인</button>
 
     </header>
   );
